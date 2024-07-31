6577ea3928a711ffd87307fb5524447550815fbf
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-07-31 19:39:52 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://afternoon-sands-47cb04422b71.herokuapp.com/';
 @Injectable({
     providedIn: 'root'
 })
