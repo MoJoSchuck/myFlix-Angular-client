abda287a4a5c60e8befe8e2740a10fef8e94640d
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-08-01 13:50:28 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,16 +14,28 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 // Component
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+const appRoutes: Routes = [
+    { path: 'welcome', component: WelcomePageComponent },
+    { path: 'movies', component: MovieCardComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
     declarations: [
         AppComponent,
         UserRegistrationFormComponent,
-        UserLoginFormComponent
+        UserLoginFormComponent,
+        MovieCardComponent,
+        WelcomePageComponent
     ],
     imports: [
         BrowserModule,
@@ -37,6 +49,8 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
         MatCardModule,
         MatFormFieldModule,
         MatSnackBarModule,
+        RouterModule.forRoot(appRoutes),
+        MatIconModule
     ],
     providers: [],
     bootstrap: [AppComponent]
