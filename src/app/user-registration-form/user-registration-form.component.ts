a259ffd47bcb6f9abb98ccfcde43949ff2fdecbf
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2024-08-06 22:36:45 +0200

    Update user-registration-form.component.ts

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -42,21 +42,42 @@ export class UserRegistrationFormComponent implements OnInit {
     // }
 
     // This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
-        // Logic for a successful user registration goes here! (To be implemented)
-        this.dialogRef.close(); // This will close the modal on success!
-        console.log(response);
-        this.snackBar.open('user registred sucessfully!', 'OK', {
+    registerUser(): void {
+        if (this.userData.Username.length < 5) {
+          this.snackBar.open('Username must be at least 5 characters long', 'OK', {
             duration: 2000
-        });
-    }, (response) => {
-        console.log(response);
-        this.snackBar.open(response, 'OK', {
+          });
+          return;
+        }
+      
+        if (!this.userData.Password) {
+          this.snackBar.open('Password is required', 'OK', {
             duration: 2000
+          });
+          return;
+        }
+      
+        if (!this.userData.Email.includes('@')) {
+          this.snackBar.open('Email does not appear to be valid', 'OK', {
+            duration: 2000
+          });
+          return;
+        }
+      
+        this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
+          this.dialogRef.close();
+          console.log(response);
+          this.snackBar.open('User registered successfully!', 'OK', {
+            duration: 2000
+          });
+        }, (response) => {
+          console.log(response);
+          this.snackBar.open(response, 'OK', {
+            duration: 2000
+          });
         });
-    });
-}
+      }
+      
 }
 
 
